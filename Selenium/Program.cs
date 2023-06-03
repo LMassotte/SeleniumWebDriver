9db5e85b1d707003380902686f314ff210e51f04
Loïc <loicx98@gmail.com>
2023-06-03 17:57:00 +0200

    deleted useless dependencies and usings

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -1,9 +1,7 @@
-﻿using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium;
-using Selenium.Utils;
+﻿using Selenium.Utils;
 
 string resultUrl = ScenariHelper.TryingToNavigateToChiefPageAsDataScientistScenario();
-Console.WriteLine("If I connect as Data Scientist and try to navigate to Chief page, i'm redirect to : " + resultUrl);
+Console.WriteLine("If I connect as Data Scientist and try to navigate to Chief page, i'm redirected to : " + resultUrl);
 
 resultUrl = ScenariHelper.TryingToNavigateToChiefPageAsChiefScenario();
 Console.WriteLine("But if I'm connected as a Chief and navigate to Chief page, the url is : " + resultUrl);
